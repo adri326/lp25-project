eeb185feb888446e2067bc2465ff85287a2e034c
Oscar DEWASMES <odewasme@pc-tc-74.utbm.local>
2021-06-08 15:23:49 +0200

    :grimacing: save function not tested

--- include/save.h
+++ include/save.h
@@ -4,6 +4,12 @@
 #include "defs.h"
 #include <stdbool.h>
 
-bool save_to_file(directory_t *root, char *path_to_target);
+int save_to_file(directory_t *root, char *path_to_target, int depth, char *path_to_current_dir);
+
+int construct_file_line(char *buffer, file_t file, char *path_to_parent_dir);
+
+int construct_dir_line(char *buffer, directory_t dir, char *path_to_parent_dir);
+
+int construct_other_line(char *buffer, file_t file, char *path_to_parent_dir);
 
 #endif // SAVE_H
