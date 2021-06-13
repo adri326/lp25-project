7b854dfc7aff42ea5558503ea66520dc83f5dddb
Adrien Burgun <adrien.burgun@orange.fr>
2021-06-13 20:43:39 +0200

    :sparkles: Only open the output file once
    The recursive bits of save_to_file got moved to save_to_file_recursive and save_to_file now only
    opens the output file and calls save_to_file_recursive.

--- include/save.h
+++ include/save.h
@@ -4,12 +4,14 @@
 #include "defs.h"
 #include <stdbool.h>
 
-int save_to_file(directory_t *root, char *path_to_target, int depth, char *path_to_current_dir, bool verbose);
+bool save_to_file(directory_t* root, const char* path_to_target, int depth, const char* current_path, bool verbose);
 
-int construct_file_line(char *buffer, file_t file, char *path_to_parent_dir);
+bool save_to_file_recursive(FILE* output, directory_t* current_dir, int depth, const char* current_path, bool verbose);
 
-int construct_dir_line(char *buffer, directory_t dir, char *path_to_parent_dir);
+bool construct_file_line(char* buffer, file_t file, const char* path_to_parent_dir);
 
-int construct_other_line(char *buffer, file_t file, char *path_to_parent_dir);
+bool construct_dir_line(char* buffer, directory_t dir, const char* path_to_parent_dir);
+
+bool construct_other_line(char* buffer, file_t file, const char* path_to_parent_dir);
 
 #endif // SAVE_H
--- src/save.c
+++ src/save.c
@@ -7,85 +7,82 @@
 #include <save.h>
 #include <defs.h>
 
-
-int save_to_file(directory_t *root, char *path_to_target, int depth, char *path_to_current_dir, bool verbose) {
-
-    //empties the target file if we are at depth 0
-    if (depth == 0) {
-        FILE *w = fopen(path_to_target, "w");
-        fclose(w);
-        if (verbose) {printf("Start of saving :\n");}
+// TODO: remove root from this
+bool save_to_file(directory_t* root, const char* path_to_target, int depth, const char* current_path, bool verbose) {
+    FILE *output = fopen(path_to_target, "w");
+    if (!output) {
+        perror("ERROR: Couldn't open target file");
+        fprintf(stderr, "Couldn't open %s!\n", path_to_target);
+        return NULL;
     }
 
-    //Put the right number of tabulations in a string
+    if (verbose) {
+        printf("Start of saving :\n");
+    }
 
+    bool res = save_to_file_recursive(output, root, depth, current_path, verbose);
+
+    fclose(output);
+
+    return res;
+}
+
+bool save_to_file_recursive(FILE* output, directory_t* current_dir, int depth, const char* current_path, bool verbose) {
+    //Put the right number of tabulations in a string (VLA!)
     char tabulations[depth+2];
-    tabulations[0] = '\0';
     int i;
     for (i=0; i<depth; i++) {
         tabulations[i] = '\t';
     }
     tabulations[i] = '\0';
 
-    //open file
-    FILE *f = fopen(path_to_target, "a");
-    if (!f){return 0;}
-
     //write the current directory informations
     char buffer[200] = {0};
-    fputs(tabulations, f);
-    construct_dir_line(buffer, *root, path_to_current_dir);
-    fputs(buffer, f);
-    fputs("\n", f);
-    
-    if (verbose) {printf("%s%s\n", tabulations, buffer);}
-    
+    fputs(tabulations, output);
+    construct_dir_line(buffer, *current_dir, current_path);
+    fputs(buffer, output);
+    fputs("\n", output);
+
+    if (verbose) {
+        printf("%s%s\n", tabulations, buffer);
+    }
+
     tabulations[i] = '\t';
     tabulations[i+1] = '\0';
 
     //writes files
-    file_t *current_file = root->files;
+    file_t *current_file = current_dir->files;
     while (current_file != NULL) {
         if (current_file->file_type == REGULAR_FILE) {
-            construct_file_line(buffer, *current_file, path_to_current_dir);
+            construct_file_line(buffer, *current_file, current_path);
+        } else {
+            construct_other_line(buffer, *current_file, current_path);
         }
-        else {
-            construct_other_line(buffer, *current_file, path_to_current_dir);
-        }
-        fputs(tabulations, f);
-        fputs(buffer, f);
-        fputs("\n", f);
-        
+        fputs(tabulations, output);
+        fputs(buffer, output);
+        fputs("\n", output);
+
         if (verbose) {printf("%s%s\n", tabulations, buffer);}
-        
+
         current_file = current_file->next_file;
     }
 
-    fclose(f);
-
     //writes directories
-    directory_t *current_dir = root->subdirs;
-    char next_dir_path[200] = {0};
-    while (current_dir != NULL) {
+    directory_t *subdir = current_dir->subdirs;
+    while (subdir != NULL) {
         //call to recurence
-        strcpy(next_dir_path, path_to_current_dir);
-        strcat(next_dir_path, "/");
-        strcat(next_dir_path, current_dir->name);
-        save_to_file(current_dir, path_to_target, depth+1, next_dir_path, verbose);
-        current_dir = current_dir->next_dir;
+        char* new_path = (char*)malloc(strlen(current_path) + strlen(subdir->name) + 2);
+        strcpy(new_path, current_path);
+        strcat(new_path, "/");
+        strcat(new_path, subdir->name);
+        save_to_file_recursive(output, subdir, depth+1, new_path, verbose);
+        subdir = subdir->next_dir;
     }
-
 
     return 0;
 }
 
-
-
-
-
-
-int construct_file_line(char *buffer, file_t file, char *path_to_parent_dir){
-
+bool construct_file_line(char* buffer, file_t file, const char* path_to_parent_dir){
     char lil_buf[200] = {0};
 
     strcpy(buffer, "1\t"); //e_type
@@ -111,8 +108,7 @@ int construct_file_line(char *buffer, file_t file, char *path_to_parent_dir){
     return 0;
 }
 
-int construct_dir_line(char *buffer, directory_t dir, char *path_to_parent_dir){
-
+bool construct_dir_line(char* buffer, directory_t dir, const char* path_to_parent_dir){
     char lil_buf[200] = {0};
 
     strcpy(buffer, "0\t"); //e_type
@@ -128,8 +124,7 @@ int construct_dir_line(char *buffer, directory_t dir, char *path_to_parent_dir){
     return 0;
 }
 
-int construct_other_line(char *buffer, file_t file, char *path_to_parent_dir){
-
+bool construct_other_line(char* buffer, file_t file, const char* path_to_parent_dir){
     char lil_buf[200] = {0};
 
     strcpy(buffer, "2\t"); //e_type
